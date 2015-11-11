cf6bd3fcae73b2df6cfdf68f9adc87433f540f9c
Mohamed-Yassine MADDOURI <m.y.maddouri@gmail.com>
2015-11-11 23:35:53 +0100

    Add tests: index/template_args, array/template_args

--- test/hyper_array.cpp
+++ test/hyper_array.cpp
@@ -1,5 +1,8 @@
+
+#include <type_traits>
+
 #include "catch/catch.hpp"
-#include <iostream>
+
 #include "../include/hyper_array/hyper_array.hpp"
 
 TEST_CASE("index/overhead", "[overhead]")
@@ -84,6 +87,18 @@ TEST_CASE("index/ctor", "[ctor]")
     }
 }
 
+TEST_CASE("index/template_args", "[template_args]")
+{
+    using hyper_array::index;
+    constexpr std::size_t dims = 3;
+
+    SECTION("Dimensions")
+    {
+        const index<dims> idx{};
+        REQUIRE(idx.dimensions() == dims);
+    }
+}
+
 TEST_CASE("array/overhead", "[overhead]")
 {
     using value_type = double;
@@ -102,6 +117,29 @@ TEST_CASE("array/overhead", "[overhead]")
     REQUIRE(sizeof(hyper_array::array<value_type, 7>) == overhead(7));
     REQUIRE(sizeof(hyper_array::array<value_type, 8>) == overhead(8));
     REQUIRE(sizeof(hyper_array::array<value_type, 9>) == overhead(9));
-
 }
 
+TEST_CASE("array/template_args", "[template_args]")
+{
+    using value_type = double;
+    constexpr std::size_t dims = 3;
+    constexpr auto order = hyper_array::array_order::COLUMN_MAJOR;
+
+    using array_type = hyper_array::array<value_type, dims, order>;
+
+    const array_type array{1, 2, 3};
+
+    SECTION("ValueType")
+    {
+        constexpr auto b = std::is_same<typename array_type::value_type, value_type>::value;
+        REQUIRE(b);
+    }
+    SECTION("Dimensions")
+    {
+        REQUIRE(array.dimensions() == dims);
+    }
+    SECTION("Order")
+    {
+        REQUIRE(array.order() == order);
+    }
+}
